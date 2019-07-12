874fb416703c220f76d4d193f21feb7da17dbd10
W. Guilherme <ravok123@gmail.com>
2019-07-12 18:13:04 -0300

    Add - Pages - Login, Register and Dashboards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', redirectTo: 'login' },
+  { path: 'login', redirectTo: 'login' },
+
+
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'aluno', loadChildren: './pages/aluno/aluno.module#AlunoPageModule' },
+  { path: 'personal', loadChildren: './pages/personal/personal.module#PersonalPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'lista', loadChildren: './pages/lista/lista.module#ListaPageModule' },
 ];
 
 @NgModule({
