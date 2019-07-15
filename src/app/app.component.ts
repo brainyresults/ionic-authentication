dbce622760f03bdf111b71564a2a40988fa48fab
W. Guilherme <ravok123@gmail.com>
2019-07-15 15:59:44 -0300

    Update App Component - Redirect and Auth Service State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+//auth
+import { AuthService } from "./services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    //auth
+    private authService: AuthService,
+    private router: Router //final auth
   ) {
     this.initializeApp();
   }
@@ -21,6 +28,14 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.authService.authenticationState.subscribe(state => {
+        if (state) {
+          this.router.navigate(["inside"]);
+        } else {
+          this.router.navigate(["login"]);
+        }
+      });
     });
   }
 }
