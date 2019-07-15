3b01b256396adc0e7a384cfa4373a617babcae75
W. Guilherme <ravok123@gmail.com>
2019-07-15 17:15:16 -0300

    Update Aluno Page - html and ts

--- src/app/pages/aluno/aluno.page.ts
+++ src/app/pages/aluno/aluno.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+//auth
+import { AuthService } from "./../../services/auth.service";
+import { Storage } from "@ionic/storage";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-aluno',
-  templateUrl: './aluno.page.html',
-  styleUrls: ['./aluno.page.scss'],
+  selector: "app-aluno",
+  templateUrl: "./aluno.page.html",
+  styleUrls: ["./aluno.page.scss"]
 })
 export class AlunoPage implements OnInit {
+  data = "";
+  constructor(
+    private authService: AuthService,
+    private storage: Storage,
+    private toastController: ToastController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loadSpecialInfo() {
+    this.authService.getSpecialData().subscribe(res => {
+      this.data = res["msg"];
+    });
   }
 
+  logout() {
+    this.authService.logout();
+  }
+
+  clearToken() {
+    // ONLY FOR TESTING!
+    this.storage.remove("access_token");
+
+    let toast = this.toastController.create({
+      message: "JWT removed",
+      duration: 3000
+    });
+    toast.then(toast => toast.present());
+  }
 }
