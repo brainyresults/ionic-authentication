eb676000dc0994dbc87b330e83647626bf0d7d51
W. Guilherme <ravok123@gmail.com>
2019-07-15 15:59:07 -0300

    Add AuthGuard to Aluno Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,37 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginPage } from './pages/login/login.page';
-
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuardService } from "./services/auth-guard.service";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', redirectTo: 'login' },
-  { path: 'login', redirectTo: 'login' },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", redirectTo: "login" },
+  { path: "login", redirectTo: "login" },
 
-
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'aluno', loadChildren: './pages/aluno/aluno.module#AlunoPageModule' },
-  { path: 'personal', loadChildren: './pages/personal/personal.module#PersonalPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'lista', loadChildren: './pages/lista/lista.module#ListaPageModule' },
-  { path: 'save-edit', loadChildren: './pages/crud/save-edit/save-edit.module#SaveEditPageModule' },
-  { path: 'delete', loadChildren: './pages/crud/delete/delete.module#DeletePageModule' },
-  { path: 'list', loadChildren: './pages/crud/list/list.module#ListPageModule' },
+  { path: "home", loadChildren: "./home/home.module#HomePageModule" },
+  { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
+  {
+    path: "aluno",
+    loadChildren: "./pages/aluno/aluno.module#AlunoPageModule",
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: "personal",
+    loadChildren: "./pages/personal/personal.module#PersonalPageModule"
+  },
+  {
+    path: "register",
+    loadChildren: "./register/register.module#RegisterPageModule"
+  },
+  { path: "lista", loadChildren: "./pages/lista/lista.module#ListaPageModule" },
+  {
+    path: "save-edit",
+    loadChildren: "./pages/crud/save-edit/save-edit.module#SaveEditPageModule"
+  },
+  {
+    path: "delete",
+    loadChildren: "./pages/crud/delete/delete.module#DeletePageModule"
+  },
+  { path: "list", loadChildren: "./pages/crud/list/list.module#ListPageModule" }
 ];
 
 @NgModule({
@@ -26,4 +40,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
