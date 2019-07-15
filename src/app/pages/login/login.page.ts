4921355eebc74bb54e7d0011645777a6a7782b6e
W. Guilherme <ravok123@gmail.com>
2019-07-15 16:32:28 -0300

    Add form builder without validators

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+//auth
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  credentialsForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.credentialsForm = this.formBuilder.group({
+      email: [""],
+      password: [""]
+    });
   }
 
+  onSubmit() {
+    this.authService.login(this.credentialsForm.value).subscribe();
+  }
+  register() {
+    this.authService.login(this.credentialsForm.value).subscribe();
+  }
 }
