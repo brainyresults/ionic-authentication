3c74bb99d1b77cc956dce146a89e1d80af615482
W. Guilherme <ravok123@gmail.com>
2019-07-15 17:15:27 -0300

    update login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { IonicModule } from '@ionic/angular';
+//auth
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { LoginPage } from './login.page';
+import { IonicModule } from "@ionic/angular";
+
+import { LoginPage } from "./login.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: LoginPage
   }
 ];
@@ -19,7 +22,9 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    //auth
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
